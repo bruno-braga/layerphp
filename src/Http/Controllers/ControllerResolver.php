f190e38838f9358bae6ece0e7b70587a33e0fdd0
Bruno <brunobraga@protonmail.com>
2025-07-20 16:50:27 -0300

    Add getRawMethodName and getMethod

--- src/Http/Controllers/ControllerResolver.php
+++ src/Http/Controllers/ControllerResolver.php
@@ -7,33 +7,58 @@ use stdClass;
 
 class ControllerResolver
 {
-	const controllerPaths = __DIR__ . '/Controllers/';
+    private const controllerPaths = __DIR__ . "/Controllers/";
 
-	private bool $canResolveController;
-	private $controllerName;
-	private string $controllerNamespace;
+    private bool $canResolveController;
+    private stdClass $controllerName;
+    private string $controllerNamespace;
+    private string $rawControllerName;
 
-	public function __construct(private string $url)
-	{
-		$this->controllerName = $this->parseControllerFileName($url);
-		$this->canResolveController = file_exists(self::controllerPaths . $this->controllerName->fileName);
-	}
+    public function __construct(private string $url)
+    {
+        $this->controllerName = $this->parseControllerFileName($url);
+        $this->canResolveController = file_exists(
+            self::controllerPaths . $this->controllerName->fileName,
+        );
+    }
 
-	private function parseControllerFileName(string $url): stdClass
-	{
-		$parsedUri = parse_url($url);
-		$rawControllerName = explode('/', $parsedUri['path'])[1];
+    private function getRawControllerName(): string
+    {
+        return $this->getRawControllerName();
+    }
 
-		$obj = new stdClass;
-		$obj->className = ucfirst($rawControllerName) . 'Controller';
-		$obj->fileName = $obj->className . '.php';
+    private function parseControllerFileName(string $url): stdClass
+    {
+        $parsedUri = parse_url($url);
+        $this->rawControllerName = explode("/", $parsedUri["path"])[1];
 
-		return $obj;
-	}
+        $obj = new stdClass();
+        $obj->className = ucfirst($this->rawControllerName) . "Controller";
+        $obj->fileName = $obj->className . ".php";
 
-	public function getController()
-	{
-		$namespace = __NAMESPACE__ . '\\' . $this->controllerName->className;
-		return new $namespace;
-	}
+        return $obj;
+    }
+
+    public function getMethod()
+    {
+        if ($this->url === "/" . $this->rawControllerName) {
+            return "index";
+        }
+
+        $explodedUrl = explode("/", $this->url);
+        $lastPath = $explodedUrl[count($explodedUrl) - 1];
+
+        return $lastPath;
+    }
+
+    public function getController()
+    {
+        $namespace = __NAMESPACE__ . "\\" . $this->controllerName->className;
+        return new $namespace();
+    }
+
+    public function getUri(): string
+    {
+        return $this->url;
+    }
 }
